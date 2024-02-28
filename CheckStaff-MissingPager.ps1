a2e55f3f92d74db9d9f909ab251e0c77524dcadc
jessevanrhyn <161541649+jessevanrhyn@users.noreply.github.com>
2024-02-28 12:52:04 +1100

    Update CheckStaff-MissingPager.ps1

--- CheckStaff-MissingPager.ps1
+++ CheckStaff-MissingPager.ps1
@@ -1,22 +1,16 @@
-﻿# Check if the ActiveDirectory module is installed
 if (Get-Module -ListAvailable -Name ActiveDirectory) {
     Write-Output "ActiveDirectory module is already installed."
 } else {
-    # Check if the script is being run as an administrator
+    # check if running as admin
     if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-        # Display a warning message
         Write-Warning "This script must be run as an administrator to install the ActiveDirectory module. Please restart the script with administrative privileges."
-        # Prompt the user to restart the script with administrative privileges
         $restart = Read-Host -Prompt 'Do you want to restart the script with administrative privileges? (y/n)'
         if ($restart -eq 'y') {
-            # Restart the script with administrative privileges
             Start-Process PowerShell -Verb RunAs -ArgumentList "-File `"$PSCommandPath`""
         }
-        # End the script
         return
     }
 
-    # Install the ActiveDirectory module
     Install-WindowsFeature -Name RSAT-AD-PowerShell
 }
 
